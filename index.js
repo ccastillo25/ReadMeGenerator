110f970446ee8f3e04ccca23cd388f17a07eaaac
Carlos Castillo <castillo25ca@gmail.com>
2021-02-18 18:34:01 -0800

    index.js adjustments

--- index.js
+++ index.js
@@ -1,72 +1,83 @@
 const inquirer = require('inquirer');
-const fs = require ('fs');
-const questions = [ 
-{
-    type: "input",
-    message: "What is the Project Title",
-    name: "projectTitle",
- },
- {
-    type: "input",
-    message: "Describe Project",
-    name: "prjectDescription",
- },
- {
-    type: "input",
-    message: "Step by step of how to install project",
-    name: "installInstruction",
- },
- {
-    type: "input",
-    message: "How do you use Project",
-    name: "Usage",
- },
- {
-    type: "input",
-    message: "Where to get support",
-    name: "Support",
- },
- {
-    type: "input",
-    message: "List those who contributed",
-    name: "Contributors",
- },
-{
-    type: "input",
-    message: "What is License Name",
-    name: "licenseName",
-},
+const fs = require('fs');
+const questions = [
+   {
+      type: "input",
+      message: "What is your GitHub user name?",
+      name: "username"
+   },
+   {
+      type: "input",
+      message: "What is your email address?",
+      name: "userEmail",
+   },
+   {
+      type: "input",
+      message: "What is the Project Title",
+      name: "projectTitle",
+   },
+   {
+      type: "input",
+      message: "Describe Project",
+      name: "projectDescription",
+   },
+   {
+      type: "input",
+      message: "Table of Content",
+      name: "Content",
+   },
+   {
+      type: "input",
+      message: "Step by step of how to install project",
+      name: "installInstruction",
+   },
+   {
+      type: "input",
+      message: "How do you use Project",
+      name: "projectUsage",
+   },
+   {
+      type: "input",
+      message: "Where to get support",
+      name: "Support",
+   },
+   {
+      type: "input",
+      message: "What command should be run to start tests?",
+      default: "`npm test`",
+      name: "projectTest",
+   }, 
+   {
+      type: "input",
+      message: "List those who contributed",
+      name: "Contributors",
+   },
+   {
+      type: "input",
+      message: "What is License Name",
+      name: "License",
+   }
 ];
 
-function captureUserInput(){
+function captureUserInput() {
    inquirer.prompt(questions)
-   .then(res => {
-      console.log(res);
-   })
+      .then(res => {
+         console.log(res);
+         writeFile("ReadMe.md", res);
+      })
 }
 captureUserInput();
 
 //.2 make function to make res into readme file
-function generateReadMe(projectTitle, res) {
-   fs.generateReadMe(projectTitle, res, err => {
-       if (err) {
+function writeFile(file, res) {
+   //.3 use fs to write a readMe.
+   fs.writeFile(file, res, function (err) {
+      if (err) {
          return console.log(err);
-       }
-     
-       console.log("Success! Your README.md file has been generated")
-   });
+      }
+      console.log("Success!");
+   })
 }
 
-const writeFileAsync = util.promisify(writeToFile);
-//.3 use fs to write a readMe.
+//writeFile();
 //.4 template literal, es6
-
-// console.log('starting');
-// console.log(userResponse);
-// const ProjectTitle = userResponse.ProjectTitle;
-// const ProjectDiscription = userResponse.ProjectDiscription;
-// const InstallInstruction = userResponse.InstallInstruction;
-// const Usage = userResponse.Usage;
-// const Support = userResponse.Support;
-// const Contributors = userResponse.Contributors;
-// const LicenseName = userResponse.LicenseName;
